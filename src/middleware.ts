bc5096ed7e671d1d74f692c94547d59ddcd5b01f
iho21 <hoivan08@gmail.com>
2024-12-13 16:25:09 -0800

    Latest changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,18 +4,42 @@
 
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { Redis } from "@upstash/redis";
+import { Ratelimit } from "@upstash/ratelimit";
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+})
+
+const rateLimit = new Ratelimit({
+  redis: redis,
+  limiter: Ratelimit.slidingWindow(4, "60 s"),
+  analytics: true,
+})
 
 export async function middleware(request: NextRequest) {
   try {
+    const ip = request.headers.get("x-forwarded-for") ?? '127.0.0.1'
 
-    const response = NextResponse.next();
-
+    const { success, limit, reset, remaining } =
+    await rateLimit.limit(ip);
+    const response = success
+      ? NextResponse.next():
+      NextResponse.json(
+        {error: 'Too Many Requests'},
+        {status: 429}
+      )
+    response.headers.set('X-RateLimit-Limit', limit.toString())
+    response.headers.set('X-RateLimit-Remaining', remaining.toString())
+    response.headers.set('X-RateLimit-Reset', reset.toString())
     return response;
 
 
 
   } catch (error) {
-
+    console.error("Error in middleware");
+    return NextResponse
 
   }
 }
